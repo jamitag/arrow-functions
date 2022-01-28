dd35b9e1c630d97737eea6f91d7893a8a3a55ceb
jamitag <jamgra12@gmail.com>
2022-01-28 11:19:54 +0000

    practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,48 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b) {
+//     //code block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(4, 5);
+
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(4, 5);
+
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => (a + b);
+
+let sum2 = addTwoNumbers2(6, 7);
+
+console.log(sum2)
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+ saySomething('Hello there');
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    '
+    <p> this is a mutiline string
+    <p></p>
+    '
+)
+
+console.log(returnMultipleLines());
+
+
